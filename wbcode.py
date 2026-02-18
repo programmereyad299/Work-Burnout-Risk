94392d49ff9f91506837638f8f76c3e1e3a87661
programmereyad299 <elsayedwagih1985@gmail.com>
2026-02-18 17:02:30 +0300

    Add files via upload

---
+++ wbcode.py
@@ -0,0 +1,46 @@
+import numpy as np
+import tensorflow as tf
+import streamlit as st
+import joblib
+
+st.set_page_config(page_title = "Work Burnout Prediction" , page_icon="🗒️")
+st.title("Work Burnout Prediction 😵‍💫😩🗒️")
+st.title("")
+
+model = tf.keras.models.load_model('wbmodel.h5')
+le0 = joblib.load('wble0.h5')
+ley = joblib.load("wbley.h5")
+
+day = st.radio("Day Type",["Weekday","Weekend"])
+
+hours = st.slider("Work Hours" , 3.0 , 12.2)
+
+screen = st.slider("Screen Time Hours" , 4.51 , 15.7)
+
+meetings = st.slider("Meetings Count" , 0 , 10)
+
+breaks = st.slider("Breaks Taken" , 1 , 5)
+
+after = st.radio("After Hours Work" ,[0,1])
+
+sleep = st.slider("Sleep Hours" , 4.5 , 10.8)
+
+task = st.slider("Task Completion Rate" , 40.0 , 107.0)
+
+burnout = st.slider("Burnout Score" , 2.5 , 144.0)
+
+
+
+
+day = le0.transform([day])[0]
+
+
+
+btn = st.sidebar.button("pred")
+
+if btn :
+    input_data = np.array([[day,hours,screen,meetings,breaks,after,sleep,task,burnout]])
+    pred = model.predict(input_data)
+    pred = np.argmax(pred)  
+    pred = ley.inverse_transform([pred])[0]
+    st.sidebar.info(pred)
